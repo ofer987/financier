d8aef4f131d6759fd2b16b8854f7d0bbffb434cb
Dan Jakob Ofer <dan@ofer.to>
2018-12-16 16:52:50 -0500

    Action-based balance sheet

---
+++ Financier.Common/Flows/Simple.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Financier.Common.Actions;
+
+namespace Financier.Common.Flows
+{
+    public class Simple 
+    {
+        public List<IAction> Actions { get; }
+
+        public DateTime StartedAt { get; }
+
+        public decimal StartingBalance { get; }
+
+        public Simple(decimal startingBalance, DateTime startedAt)
+        {
+            StartingBalance = startingBalance;
+            StartedAt = startedAt;
+        }
+
+        public decimal GetBalance(DateTime at)
+        {
+            return Actions
+                .Where(action => action.At <= at)
+                .OrderBy(action => action.At)
+                .Aggregate(StartingBalance, (balance, action) => balance + action.TotalBy(at));
+        }
+    }
+}
