c12dd30945bd2d05d3579ca9d720d75040b3061d
Dan Jakob Ofer <dan@ofer.to>
2018-12-24 12:55:40 -0500

    Test Financier.Common

---
+++ Financier.Tester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Financier.Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
