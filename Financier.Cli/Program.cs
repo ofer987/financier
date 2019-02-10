62a7f805964f6c8a820547305a2a3c1366488afa
Dan Jakob Ofer <dan@ofer.to>
2019-02-10 18:49:43 -0500

    Add a CLI application to import statements

---
+++ Financier.Cli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Financier.Common;
+
+namespace Financier.Cli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var postedAt = GetPostedAt(args);
+            var stream = System.IO.File.OpenRead(GetStatementPath(args));
+
+            new StatementImporter().Import(Guid.NewGuid(), postedAt, stream);
+        }
+
+        public static DateTime GetPostedAt(IReadOnlyList<string> args)
+        {
+            return DateTime.ParseExact(args[0], "yyyyMMdd", null);
+        }
+
+        public static string GetStatementPath(IReadOnlyList<string> args)
+        {
+            return args[1];
+        }
+    }
+}
