13738b575c041e3b97137c28f9652537a3c21efa
Dan Jakob Ofer <dan@ofer.to>
2019-03-22 21:17:25 -0400

    View (i.e., list) models

---
+++ Financier.Cli.Listings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+using Financier.Common;
+
+namespace Financier.Cli.Listings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context.Environment = Environments.Dev;
+            Console.WriteLine("Hello World!");
+
+            using (var db = new Context())
+            {
+                var selectedStatement = db.Statements
+                    .Include(stmt => stmt.Items)
+                    .First(stmt => stmt.Id == Guid.Parse(args[0]));
+
+                Console.WriteLine(selectedStatement);
+            }
+        }
+    }
+}
