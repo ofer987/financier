218dcb95bf4eff927d0570b0e1799e06710ba529
Dan Jakob Ofer <dan@ofer.to>
2019-03-30 14:02:28 -0400

    Rename tags

--- Financier.Cli.Deletion/Program.cs
+++ Financier.Cli.Deletion/Program.cs
@@ -8,7 +8,7 @@ namespace Financier.Cli.Deletion
 {
     public class Program
     {
-        [OptionAttribute("-t|--type", CommandOptionType.SingleValue)]
+        [OptionAttribute("-m|--model", CommandOptionType.SingleValue)]
         public string _modelType { get; }
         public string ModelType => (_modelType ?? string.Empty).Trim().ToLower();
 
@@ -16,36 +16,23 @@ namespace Financier.Cli.Deletion
         private string _modelName { get; }
         public string ModelName => (_modelName ?? string.Empty).Trim();
 
+        [OptionAttribute("-t|--target-name", CommandOptionType.SingleOrNoValue)]
+        public (bool HasValue, string Value) _targetName { get; }
+        public string TargetName => (_targetName.Value ?? string.Empty).Trim();
+
         public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
 
         private void OnExecute()
         {
             try
             {
-                switch (ModelType)
+                if (_targetName.HasValue)
                 {
-                    case "card":
-                        using (var db = new Context())
-                        {
-                            db.Cards
-                                .First(card => card.Id == Guid.Parse(ModelName))
-                                .Delete();
-                        }
-                        break;
-                    case "statement":
-                        using (var db = new Context())
-                        {
-                            db.Statements
-                                .First(statement => statement.Id == Guid.Parse(ModelName))
-                                .Delete();
-                        }
-                        break;
-                    default:
-                        Console.WriteLine($"Invalid value {ModelType}");
-                        Console.WriteLine("Valid values:");
-                        Console.WriteLine("Card");
-                        Console.WriteLine("Statement");
-                        break;
+                    Rename();
+                }
+                else
+                {
+                    Delete();
                 }
             }
             catch (Exception exception)
@@ -53,6 +40,64 @@ namespace Financier.Cli.Deletion
                 Console.WriteLine("Error!");
                 Console.WriteLine(exception.Message);
                 Console.WriteLine(exception.StackTrace);
+            }
+        }
+
+        private void Rename()
+        {
+            switch (ModelType)
+            {
+                case "tag":
+                    using (var db = new Context())
+                    {
+                        db.Tags
+                            .First(tag => tag.Name == ModelName)
+                            .Rename(TargetName);
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"Invalid value {ModelType}");
+                    Console.WriteLine("Valid values:");
+                    Console.WriteLine("Tag");
+                    break;
+            }
+        }
+
+        private void Delete()
+        {
+            switch (ModelType)
+            {
+                case "card":
+                    using (var db = new Context())
+                    {
+                        db.Cards
+                            .First(card => card.Id == Guid.Parse(ModelName))
+                            .Delete();
+                    }
+                    break;
+                case "statement":
+                    using (var db = new Context())
+                    {
+                        db.Statements
+                            .First(statement => statement.Id == Guid.Parse(ModelName))
+                            .Delete();
+                    }
+                    break;
+                case "tag":
+                    using (var db = new Context())
+                    {
+                        db.Tags
+                            .First(tag => tag.Name == ModelName)
+                            .Delete();
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"Invalid value {ModelType}");
+                    Console.WriteLine("Valid values:");
+                    Console.WriteLine("Card");
+                    Console.WriteLine("Statement");
+                    Console.WriteLine("Tag");
+                    break;
             }
         }
     }
