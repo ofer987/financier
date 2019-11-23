16a81af09200078c0267f3e212bba4e6ee93474f
Dan Jakob Ofer <dan@ofer.to>
2019-11-23 11:53:11 -0500

    Bug fix: remove repeated graphql field

--- Financier.Web/GraphQL/Items/ItemType.cs
+++ Financier.Web/GraphQL/Items/ItemType.cs
@@ -12,7 +12,6 @@ namespace Financier.Web.GraphQL.Items
             Field(t => t.Id, nullable: false, type: typeof(IdGraphType));
             Field(t => t.Description, nullable: false, type: typeof(StringGraphType));
             Field(t => t.At, nullable: false, type: typeof(DateGraphType));
-            Field(t => t.At, nullable: false, type: typeof(DateGraphType));
             Field("amount", t => t.TheRealAmount.ToString("#0.00"), nullable: false, type: typeof(StringGraphType));
             Field(t => t.ItemId, nullable: false, type: typeof(StringGraphType));
         }
