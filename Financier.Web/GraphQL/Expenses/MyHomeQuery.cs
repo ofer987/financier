5fec240c93f5682f0e0b2ef54e6aac4e8c3adbe8
Dan Jakob Ofer <dan@ofer.to>
2020-01-18 11:09:06 -0500

    MyHomeQuery.getFixedMortgagePaymentsWithPrepayments
    Added a new GraphQL endpoint
    MyHomeQuery.getFixedMortgagePaymentsWithPrepayments

--- Financier.Web/GraphQL/Expenses/MyHomeQuery.cs
+++ Financier.Web/GraphQL/Expenses/MyHomeQuery.cs
@@ -29,6 +29,71 @@ namespace Financier.Web.GraphQL.Expenses
         public MyHomeQuery()
         {
             Field<ListGraphType<MonthlyPaymentType>>(
+                "getFixedMortgagePaymentsWithPrepayments",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<DecimalGraphType>>
+                    {
+                        Name = Keys.InitialCash
+                    },
+                    new QueryArgument<NonNullGraphType<DecimalGraphType>>
+                    {
+                        Name = Keys.InitialDebt
+                    },
+                    new QueryArgument<NonNullGraphType<DateGraphType>>
+                    {
+                        Name = Keys.PurchasedAt
+                    },
+                    new QueryArgument<StringGraphType>
+                    {
+                        Name = Keys.Name,
+                        DefaultValue = "Loud Train Lady"
+                    },
+                    new QueryArgument<NonNullGraphType<DecimalGraphType>>
+                    {
+                        Name = Keys.DownPayment
+                    },
+                    new QueryArgument<NonNullGraphType<DecimalGraphType>>
+                    {
+                        Name = Keys.MortgageAmount
+                    },
+                    new QueryArgument<NonNullGraphType<DecimalGraphType>>
+                    {
+                        Name = Keys.InterestRate
+                    },
+                    new QueryArgument<IntGraphType>
+                    {
+                        Name = Keys.AmortisationPeriodInMonths,
+                        DefaultValue = 300
+                    }
+                ),
+                resolve: context =>
+                {
+                    var homeName = context.GetArgument<string>(Keys.Name);
+                    var purchasedAt = context.GetArgument<DateTime>(Keys.PurchasedAt);
+                    var downPayment = context.GetArgument<decimal>(Keys.DownPayment);
+                    var home = new Home(homeName, purchasedAt, downPayment);
+
+                    var baseValue = context.GetArgument<decimal>(Keys.MortgageAmount);
+                    var interestRate = context.GetArgument<decimal>(Keys.InterestRate);
+                    var amortisationPeriodInMonths = context.GetArgument<int>(Keys.AmortisationPeriodInMonths);
+                    var mortgage = new FixedRateMortgage(home, baseValue, interestRate, amortisationPeriodInMonths);
+
+                    var cashflow = CreateCashFlow();
+
+                    var initialCash = context.GetArgument<decimal>(Keys.InitialCash);
+                    var initialDebt = context.GetArgument<decimal>(Keys.InitialDebt);
+                    var financialStatement = MyHome.BuildStatementWithPrepaybleMortgage(
+                        mortgage,
+                        cashflow,
+                        initialCash,
+                        initialDebt
+                    );
+
+                    return financialStatement.Mortgage.GetMonthlyPayments();
+                }
+            );
+
+            Field<ListGraphType<MonthlyPaymentType>>(
                 "getFixedMortgagePayments",
                 arguments: new QueryArguments(
                     new QueryArgument<NonNullGraphType<DecimalGraphType>>
