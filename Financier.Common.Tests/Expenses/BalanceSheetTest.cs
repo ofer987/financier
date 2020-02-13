26a49fda08ce1fc5ccbeb47618312d0545b99c34
Dan Jakob Ofer <dan@ofer.to>
2020-02-12 21:18:40 -0500

    Fix BalanceSheet test cases

--- Financier.Common.Tests/Expenses/BalanceSheetTest.cs
+++ Financier.Common.Tests/Expenses/BalanceSheetTest.cs
@@ -34,28 +34,9 @@ namespace Financier.Common.Tests.Expenses
         }
 
         // TODO: calculate just the mortgage payments
-        [TestCase(InflationTypes.NoopInflation, 2019, 1, 1, 1584.84)]
-        [TestCase(InflationTypes.CompoundYearlyInflation, 2019, 1, 1, 1584.84)]
-        [TestCase(2019, 1, 15, -330023.76)]
-        [TestCase(2019, 1, 31, -328586.00)]
-        [TestCase(2019, 2, 1, -328496.14)]
-        [TestCase(2019, 2, 2, -327686.18)]
-        [TestCase(2019, 12, 31, 32800.00)]
-        [TestCase(2020, 1, 1, 1584.84)]
-        [TestCase(2020, 1, 2, -285628.46 - 4000 + 89.86 * 1)]
-        [TestCase(2020, 2, 1, -285628.46 - 4000 + 89.86 * 31)]
-        [TestCase(2020, 12, 31, 32800.00)]
-        [TestCase(2021, 1, 1, -243575.83 - 4000 + 89.86 * 0 + (89.86 * 366 - 32800))]
-        [TestCase(2026, 1, 1, -11410.01)]
-        [TestCase(2026, 2, 1, -7059.99)]
-        [TestCase(2026, 3, 1, -2975.42)]
-        [TestCase(2026, 4, 1, 1382.87)]
-        [TestCase(2026, 5, 1, 5655.47)]
-        [TestCase(2026, 6, 1, 9746.38)]
-        [TestCase(2026, 7, 1, 12442.18)]
-        [TestCase(2026, 8, 1, 15227.84)]
-        [TestCase(2026, 9, 1, 18013.50)]
-        [TestCase(2026, 10, 1, 1584.84)]
+        [TestCase(InflationTypes.NoopInflation, 2019, 1, 1, 10000.00 + 82000.00 + 89.86 * 0)]
+        [TestCase(InflationTypes.NoopInflation, 2019, 1, 2, 10000.00 + 82000.00 + 718.20 + 89.86 * 1)]
+        [TestCase(InflationTypes.NoopInflation, 2019, 1, 15, 10000.00 + 82000.00 + 718.20 + 89.86 * 14)]
         public void Test_GetAssets(InflationTypes inflationType, int year, int month, int day, decimal expected)
         {
             var inflation = Inflations.GetInflation(inflationType);
