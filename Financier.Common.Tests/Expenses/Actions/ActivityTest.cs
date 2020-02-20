b08f821074c5094e9a84c9305091b197feed1852
Dan Jakob Ofer <dan@ofer.to>
2020-02-20 10:58:29 -0500

    Get products that are owned by

--- Financier.Common.Tests/Expenses/Actions/ActivityTest.cs
+++ Financier.Common.Tests/Expenses/Actions/ActivityTest.cs
@@ -10,11 +10,15 @@ namespace Financier.Common.Tests.Expenses.ActionTests
     public class ActivityTest
     {
         public Product Television { get; private set; }
+        public Product Stand { get; private set; }
+        public Product House { get; private set; }
 
         [SetUp]
         public void Init()
         {
-            Television = new SimpleProduct("house", new Money(40.00M, new DateTime(2020, 1, 1)));
+            Television = new SimpleProduct("television", new Money(40.00M, new DateTime(2020, 1, 1)));
+            Stand = new SimpleProduct("stand", new Money(20.00M, new DateTime(2020, 1, 1)));
+            House = new SimpleProduct("stand", new Money(5000.00M, new DateTime(2020, 1, 1)));
         }
 
         [Test]
@@ -101,6 +105,56 @@ namespace Financier.Common.Tests.Expenses.ActionTests
             {
                 activity.Sell(Television, new Money(90.00M, new DateTime(2020, 1, 1)), new DateTime(2019, 12, 1));
             });
+        }
+
+        [Test]
+        public void Test_GetOwnedProducts()
+        {
+            var activity = new Activity();
+
+            activity.Buy(Television, new DateTime(2020, 1, 1));
+            activity.Sell(Television, new Money(50.00M, new DateTime(2020, 2, 1)), new DateTime(2020, 2, 1));
+            activity.Buy(Television, new DateTime(2020, 3, 1));
+            activity.Sell(Television, new Money(60.00M, new DateTime(2020, 1, 1)), new DateTime(2020, 4, 1));
+            activity.Buy(Television, new DateTime(2020, 8, 1));
+
+            activity.Buy(Stand, new DateTime(2020, 3, 20));
+            activity.Sell(Stand, new Money(100.00M, new DateTime(2022, 2, 1)), new DateTime(2022, 2, 1));
+
+            activity.Buy(House, new DateTime(2022, 1, 1));
+
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2019, 12, 31)),
+                Is.EquivalentTo(Enumerable.Empty<Product>())
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 1, 1)),
+                Is.EquivalentTo(new[] { Television })
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 1, 2)),
+                Is.EquivalentTo(new[] { Television })
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 2, 1)),
+                Is.EquivalentTo(Enumerable.Empty<Product>())
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 2, 2)),
+                Is.EquivalentTo(Enumerable.Empty<Product>())
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 3, 2)),
+                Is.EquivalentTo(new[] { Television })
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2020, 3, 21)),
+                Is.EquivalentTo(new[] { Television, Stand })
+            );
+            Assert.That(
+                activity.GetOwnedProducts(new DateTime(2022, 1, 2)),
+                Is.EquivalentTo(new[] { Television, Stand, House })
+            );
         }
     }
 }
