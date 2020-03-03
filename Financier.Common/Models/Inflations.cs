4b99cc36daa22dc6fe9c5e467c54448de7f42e21
Dan Jakob Ofer <dan@ofer.to>
2020-03-02 21:07:19 -0500

    Can pass arguments to factory function

--- Financier.Common/Models/Inflations.cs
+++ Financier.Common/Models/Inflations.cs
@@ -4,14 +4,21 @@ namespace Financier.Common.Models
 {
     public static class Inflations
     {
-        public static IInflation GetInflation(InflationTypes type)
+        public static IInflation GetInflation(InflationTypes type, params object[] args)
         {
             switch (type)
             {
                 case InflationTypes.NoopInflation:
                     return new NoopInflation();
                 case InflationTypes.CompoundYearlyInflation:
-                    return new CompoundYearlyInflation();
+                    if (args.Length >= 1)
+                    {
+                        return new CompoundYearlyInflation((decimal)args[0]);
+                    }
+                    else
+                    {
+                        return new CompoundYearlyInflation();
+                    }
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type));
             }
