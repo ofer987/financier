04158ad8df4e203c4fbf68d07f53cc456a53f0d5
Dan Jakob Ofer <dan@ofer.to>
2020-03-07 20:33:00 -0500

    Fix error message

--- Financier.Common/Liabilities/InvalidDownPaymentRateException.cs
+++ Financier.Common/Liabilities/InvalidDownPaymentRateException.cs
@@ -4,7 +4,7 @@ namespace Financier.Common.Liabilities
 {
     public class InvalidDownPaymentRateException : ArgumentOutOfRangeException
     {
-        public InvalidDownPaymentRateException(decimal rate) : base(nameof(rate), rate, $"Should be between {InsuredMortgage.MinimumInsuranceRate}% and {InsuredMortgage.MaximumInsuranceRate}%")
+        public InvalidDownPaymentRateException(decimal rate) : base(nameof(rate), rate, $"Should be between {InsuredMortgage.MinimumInsuranceRate * 100}% and {InsuredMortgage.MaximumInsuranceRate * 100}%")
         {
         }
     }
