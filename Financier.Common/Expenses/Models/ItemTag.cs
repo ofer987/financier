604d4e4db211a5a35e02cf304252829fb185e5f8
Dan Jakob Ofer <dan@ofer.to>
2020-07-12 18:14:37 -0400

    Remove whitespace

--- Financier.Common/Expenses/Models/ItemTag.cs
+++ Financier.Common/Expenses/Models/ItemTag.cs
@@ -7,11 +7,8 @@ namespace Financier.Common.Expenses.Models
     public class ItemTag
     {
         public Guid ItemId { get; set; }
-
         public Item Item { get; set; }
-
         public Guid TagId { get; set; }
-
         public Tag Tag { get; set; }
 
         public void Delete()
