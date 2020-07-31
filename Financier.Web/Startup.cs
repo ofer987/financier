605427568566c231c2a819ffc8a3155571b3330c
Dan Jakob Ofer <dan@ofer.to>
2020-07-31 11:43:02 -0400

    Add Payment GraphQL routes

--- Financier.Web/Startup.cs
+++ Financier.Web/Startup.cs
@@ -52,6 +52,7 @@ namespace Financier.Web
             services.AddScoped<TagSchema>();
             services.AddScoped<ItemQuerySchema>();
             services.AddScoped<FixedRateMortgageSchema>();
+            services.AddScoped<PaymentSchema>();
 
             // Add GraphQL
             services
@@ -98,6 +99,9 @@ namespace Financier.Web
             app.UseGraphQL<ItemSchema>("/graphql/items");
             app.UseGraphQL<CashFlowSchema>("/graphql/cash-flows");
             app.UseGraphQL<ItemQuerySchema>("/graphql/item-queries");
+            app.UseGraphQL<PaymentSchema>("/graphql/payments");
+
+            // Remove!
             app.UseGraphQL<FixedRateMortgageSchema>("/graphql/fixed-rate-mortgage");
 
             // app.UseWebSockets();
