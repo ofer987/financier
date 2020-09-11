a5307701990ed73ca19a9bd1d8e69fc6dc1f8457
Dan Jakob Ofer <dan@ofer.to>
2020-09-11 14:41:25 -0400

    Created Identity app
    The identity app is used to,
    1. Authenticate users;
    2. Authorize users to perform various activities;
    3. Host the SPA;
    4. Redirect HTTP API requests to fetch user-specific data. For example,
       fetch all statements that belong to the user who is logged in. So in
       effect, the Identity app will behave like a reverse proxy.

---
+++ Financier.Web.Identity/Controllers/ExpensesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Financier.Web.Identity.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly ILogger<ExpensesController> _logger;
+
+        public ExpensesController(ILogger<ExpensesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{internalRoute}")]
+        public string Get(string internalRoute)
+        {
+            return $"Internal route is ({internalRoute})";
+        }
+    }
+}
