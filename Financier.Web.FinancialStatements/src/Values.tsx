05c5f1a859cc69bfd79129d4fbd09300b28ea0cf
Dan Jakob Ofer <dan@ofer.to>
2021-09-26 13:30:33 -0400

    Refactor Values

--- Financier.Web.FinancialStatements/src/Values.tsx
+++ Financier.Web.FinancialStatements/src/Values.tsx
@@ -3,6 +3,7 @@ import _ from "underscore";
 import Value from "./Value";
 import { Listing } from "./Listing";
 import NullListing from "./NullListing";
+import FilterableController from "./FilterableController";
 
 interface Props {
   debits: Listing[];
@@ -10,65 +11,7 @@ interface Props {
   enabledTags: string[];
 }
 
-class Values extends React.Component<Props> {
-  constructor(props: Props) {
-    super(props);
-  }
-
-  get enabledTags(): string[][] {
-    const creditTags = this.props.credits.map(item => item.tags);
-    const debitTags = this.props.debits.map(item => item.tags);
-
-    let tagsList = creditTags.concat(debitTags);
-    tagsList = _.uniq(tagsList);
-    tagsList = tagsList.filter(tags => {
-      return tags.find(tag => typeof (this.enabledTagNames.find(t => t == tag)) != "undefined");
-    });
-
-    return tagsList;
-  }
-
-  get enabledTagNames(): string[] {
-    return this.props.enabledTags;
-  }
-
-  get totalCredits(): number {
-    var amounts = this.props.credits
-      .filter(credit => {
-        const searchedTag = credit.tags
-          .find(tag => _.contains(this.props.enabledTags, tag));
-
-        return typeof (searchedTag) != "undefined";
-      })
-      .map(item => item.amount);
-    var amount = 0;
-
-    return _.reduce(amounts, (total, amount) => total + amount) || 0;
-  }
-
-  get totalDebits(): number {
-    var amounts = this.props.debits
-      .filter(debit => {
-        const searchedTag = debit.tags
-          .find(tag => _.contains(this.props.enabledTags, tag));
-
-        return typeof (searchedTag) != "undefined";
-      })
-      .map(item => item.amount);
-
-    return _.reduce(amounts, (total, amount) => total + amount) || 0;
-  }
-
-  findListingByTags(models: Listing[], tags: string[]): Listing {
-    var result = models.find(item => item.tags == tags);
-
-    if (typeof (result) == "undefined") {
-      return new NullListing();
-    }
-
-    return result;
-  }
-
+class Values extends FilterableController {
   render() {
     return (
       <div className="values">
