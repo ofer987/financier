add129220aaee0c7875f1fc7589b65ec4a2a906c
Dan Jakob Ofer <dan@ofer.to>
2021-09-26 13:30:33 -0400

    Refactor Values

---
+++ Financier.Web.FinancialStatements/src/FilterableController.tsx
@@ -0,0 +1,93 @@
+import * as React from "react";
+import _ from "underscore";
+
+import Value from "./Value";
+import { Listing } from "./Listing";
+import CashFlowModel from "./CashFlowModel";
+import NullListing from "./NullListing";
+
+interface Props {
+  debits: Listing[];
+  credits: Listing[];
+  enabledTags: string[];
+}
+
+abstract class FilterableController extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  get enabledCredits(): Listing[] {
+    return this.getEnabledListings(this.props.credits);
+  }
+
+  get enabledDebits(): Listing[] {
+    return this.getEnabledListings(this.props.debits);
+  }
+
+  get enabledTags(): string[][] {
+    const creditTags = this.props.credits.map(item => item.tags);
+    const debitTags = this.props.debits.map(item => item.tags);
+
+    let tagsList = creditTags.concat(debitTags);
+    tagsList = _.uniq(tagsList);
+    tagsList = tagsList.filter(tags => {
+      return tags.find(tag => typeof (this.enabledTagNames.find(t => t == tag)) != "undefined");
+    });
+
+    return tagsList;
+  }
+
+  get enabledTagNames(): string[] {
+    return this.props.enabledTags;
+  }
+
+  get totalCredits(): number {
+    var amounts = this.props.credits
+      .filter(credit => {
+        const searchedTag = credit.tags
+          .find(tag => _.contains(this.props.enabledTags, tag));
+
+        return typeof (searchedTag) != "undefined";
+      })
+      .map(item => item.amount);
+    var amount = 0;
+
+    return _.reduce(amounts, (total, amount) => total + amount) || 0;
+  }
+
+  get totalDebits(): number {
+    var amounts = this.props.debits
+      .filter(debit => {
+        const searchedTag = debit.tags
+          .find(tag => _.contains(this.props.enabledTags, tag));
+
+        return typeof (searchedTag) != "undefined";
+      })
+      .map(item => item.amount);
+
+    return _.reduce(amounts, (total, amount) => total + amount) || 0;
+  }
+
+  findListingByTags(models: Listing[], tags: string[]): Listing {
+    var result = models.find(item => item.tags == tags);
+
+    if (typeof (result) == "undefined") {
+      return new NullListing();
+    }
+
+    return result;
+  }
+
+  private getEnabledListings(listings: Listing[]): Listing[] {
+    return listings
+      .filter(listing => {
+        const tagNames = this.enabledTagNames
+          .filter(tagName => CashFlowModel.hasTag(tagName, listing));
+
+        return tagNames.length > 0;
+      });
+  }
+}
+
+export default FilterableController;
