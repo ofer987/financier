7b0e87e142b9dd00c0817f167dbdb2e3a930e5a4
Dan Jakob Ofer <dan@ofer.to>
2021-10-17 11:12:02 -0400

    Set Error Boundary; Create new Links

--- Financier.Web.FinancialStatements/src/App.tsx
+++ Financier.Web.FinancialStatements/src/App.tsx
@@ -3,20 +3,23 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, useParams, useLocation, useRouteMatch, Link, NavLink } from "react-router-dom";
 
 import Welcome from "./Welcome";
+import WelcomeError from "./WelcomeError";
 import MonthlyRoute from "./MonthlyRoute";
 import DetailedRoute from "./DetailedRoute";
 
 function App() {
   return (
     <div>
-      <h1 className="header">Financier</h1>
+      <h1 className="main-header">Financier</h1>
       <Router>
         <Switch>
           <Route path="/detailed-view/year/:year/month/:month" component={DetailedRoute}>
           </Route>
           <Route path="/monthly-view/year/:year" component={MonthlyRoute} />
           <Route path="/">
-            <Welcome />
+            <WelcomeError>
+              <Welcome />
+            </WelcomeError>
           </Route>
         </Switch>
       </Router>
--- Financier.Web.FinancialStatements/src/Welcome.tsx
+++ Financier.Web.FinancialStatements/src/Welcome.tsx
@@ -1,62 +1,138 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import * as d3TimeFormat from "d3-time-format";
+
 import "react-datepicker/dist/react-datepicker.css";
 
 interface Props {}
 interface State {
-  date: Date;
+  isValid: boolean;
+  startDate: Date;
+  endDate: Date;
 }
 
-class MonthPicker extends React.Component<Props, State> {
-  get date(): Date {
-    return this.state.date;
+class Welcome extends React.Component<Props, State> {
+  get isValid() {
+    return (parseInt(`${this.endYear}${this.endMonth}`) >= parseInt(`${this.startYear}${this.startMonth}`));
+  }
+
+  get startYear(): string {
+    return d3TimeFormat.timeFormat("%Y")(this.startDate);
+  }
+
+  get startMonth(): string {
+    return d3TimeFormat.timeFormat("%m")(this.startDate);
+  }
+
+  get startDate(): Date {
+    return this.state.startDate;
+  }
+
+  get endYear(): string {
+    return d3TimeFormat.timeFormat("%Y")(this.endDate);
+  }
+
+  get endMonth(): string {
+    return d3TimeFormat.timeFormat("%m")(this.endDate);
+  }
+
+  get endDate(): Date {
+    return this.state.endDate;
   }
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      date: new Date()
+      isValid: true,
+      startDate: new Date(),
+      endDate: new Date()
     };
-
-    // const [startDate, setStartDate] = React.useState(new Date());
   }
 
-  setDate(newDate: Date): void {
+  setStartDate(newDate: Date): void {
     console.log(`changing date: ${newDate}`);
     this.setState({
-      date: newDate
+      startDate: newDate
     });
+  }
+
+  setEndDate(newDate: Date): void {
+    console.log(`changing date: ${newDate}`);
+    this.setState({
+      endDate: newDate
+    });
+  }
+
+  private renderErrors() {
+    if (this.isValid) {
+      return (
+        <div></div> 
+      );
+    }
+
+    return (
+      <div>
+        To Date should be after From Date
+      </div>
+    );
+  }
+
+  private renderSubmission() {
+    if (this.isValid) {
+      console.log("is valid");
+      return (
+        <a href={`/monthly-view/year/${this.startYear}`}>View</a>
+      );
+    }
+
+    return (
+      <div></div>
+    );
   }
 
   render() {
     return (
-      <div className="date-range">
-        <div className="date-picker start">
-          <div className="name">From</div>
-          <DatePicker 
-            id="start-date-picker"
-            selected={this.date} 
-            dateFormat="LLLL/yyyy" 
-            showMonthYearPicker 
-            // @ts-ignore 
-            onChange={(date) => this.setDate(date)} 
-          />
+      <div className="welcome">
+        <div className="errors">
+          {this.renderErrors()}
         </div>
-        <div className="date-picker end">
-          <div className="name">To</div>
-          <DatePicker 
-            id="end-date-picker"
-            selected={this.date} 
-            dateFormat="LLLL/yyyy" 
-            showMonthYearPicker 
-            // @ts-ignore 
-            onChange={(date) => this.setDate(date)} 
-          />
+
+        <div className="date-range">
+          <div className="date-picker start">
+            <div className="container">
+              <div className="name">From</div>
+              <DatePicker
+                id="start-date-picker"
+                selected={this.startDate}
+                dateFormat="LLLL/yyyy"
+                showMonthYearPicker
+                // @ts-ignore
+                onChange={(date) => this.setStartDate(date)}
+              />
+            </div>
+          </div>
+          <div className="date-picker end">
+            <div className="container">
+              <div className="name">To</div>
+              <DatePicker
+                id="end-date-picker"
+                selected={this.endDate}
+                dateFormat="LLLL/yyyy"
+                showMonthYearPicker
+                // @ts-ignore
+                onChange={(date) => this.setEndDate(date)}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="submission">
+          {this.renderSubmission()}
         </div>
       </div>
     );
   }
 }
 
-export default MonthPicker;
+export default Welcome;
