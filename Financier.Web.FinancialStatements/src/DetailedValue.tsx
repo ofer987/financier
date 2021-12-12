47b463cdafd54cbe1b5544126286e2a0fe12b88b
Dan Jakob Ofer <dan@ofer.to>
2021-12-12 09:38:46 -0500

    Fix the Detailed CashFlow page

--- Financier.Web.FinancialStatements/src/DetailedValue.tsx
+++ Financier.Web.FinancialStatements/src/DetailedValue.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import _  from "underscore";
 import lodash from "lodash";
-import { Listing } from "./Listing";
-import { DetailedListing } from "./DetailedCashFlow";
+import { DetailedRecord } from "./DetailedRecord";
 
-// interface Props {
-//   listing: Listing;
-// }
+interface Props {
+  record: DetailedRecord;
+}
 
-class DetailedValue extends React.Component<DetailedListing> {
+class DetailedValue extends React.Component<Props> {
   render() {
     return (
       <div className="item" id={this.name} key={this.name}>
@@ -32,17 +31,17 @@ class DetailedValue extends React.Component<DetailedListing> {
     // var tags = this.props.listing.tags.concat(
     //   this.props.debit.tags);
 
-    return _.uniq(this.props.tags)
+    return _.uniq(this.props.record.tags)
       .map(tag => lodash.startCase(tag))
       .join(", ");
   }
 
   get creditAmount(): string {
-    return this.props.listing.creditAmount.toFixed(2);
+    return this.props.record.amount.credit.toFixed(2);
   }
 
   get debitAmount(): string {
-    return this.props.listing.debitAmount.toFixed(2);
+    return this.props.record.amount.debit.toFixed(2);
   }
 }
 
