9b14bf7e2c06ed168eb0719c7bc87e38e854490c
Dan Jakob Ofer <dan@ofer.to>
2021-12-22 15:11:58 -0500

    Project future credits and debits

---
+++ Financier.Common.Tests/Expenses/ProjectedCashFlowTests.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using NUnit.Framework;
+
+using Financier.Common.Expenses;
+using Financier.Common.Expenses.Models;
+
+namespace Financier.Common.Tests.Expenses
+{
+    public class ProjectedCashFlowTests : InitializedDatabaseTests
+    {
+        public static IEnumerable ProjectionFailCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new DateTime(2019, 9, 1),
+                    new DateTime(2019, 9, 15),
+                    new DateTime(2020, 12, 1)
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 9, 1),
+                    new DateTime(2019, 9, 15),
+                    new DateTime(2020, 10, 1)
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 9, 1),
+                    new DateTime(2019, 9, 30),
+                    new DateTime(2020, 10, 1)
+                );
+            }
+        }
+
+        public static IEnumerable CreditProjectionCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 8, 1),
+                    new DateTime(2019, 9, 1),
+                    2800.00M
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 10, 1),
+                    new DateTime(2019, 11, 1),
+                    (1000000M + 2000M + 800M) / 3
+                );
+            }
+        }
+
+        public static IEnumerable FutureProjectionFailCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new DateTime(2019, 9, 1),
+                    new DateTime(2019, 10, 1),
+                    new DateTime(2019, 10, 1)
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 10, 1),
+                    new DateTime(2019, 8, 1)
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 10, 1),
+                    new DateTime(2019, 6, 1)
+                );
+            }
+        }
+
+        public static IEnumerable DebitProjectionCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 8, 1),
+                    new DateTime(2019, 9, 1),
+                    98.25M + 4.20M + 10.00M
+                );
+
+                yield return new TestCaseData(
+                    new DateTime(2019, 7, 1),
+                    new DateTime(2019, 10, 1),
+                    new DateTime(2019, 11, 1),
+                    (98.25M + 4.20M + 10.00M) / 3
+                );
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ProjectionFailCases))]
+        public void Test_Expenses_ProjectedCashFlow_Fails(
+            DateTime startAt,
+            DateTime endAt,
+            DateTime projectedAt)
+        {
+            Assert.That(() => new ProjectedCashFlow(startAt, endAt), Throws.ArgumentException);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CreditProjectionCases))]
+        public void Test_Expenses_ProjectedCashFlow_GetProjectedCreditAt(
+            DateTime startAt,
+            DateTime endAt,
+            DateTime projectedAt,
+            decimal expectedAmount)
+        {
+            var cashFlow = new ProjectedCashFlow(startAt, endAt);
+
+            Assert.That(cashFlow.GetProjectedCreditAt(projectedAt), Is.EqualTo(expectedAmount));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(FutureProjectionFailCases))]
+        public void Test_Expenses_ProjectedCashFlow_GetProjectedCreditAt_Fails(
+            DateTime startAt,
+            DateTime endAt,
+            DateTime projectedAt)
+        {
+            var cashFlow = new ProjectedCashFlow(startAt, endAt);
+
+            Assert.That(() => cashFlow.GetProjectedCreditAt(projectedAt), Throws.ArgumentException);
+            Assert.That(() => cashFlow.GetProjectedDebitAt(projectedAt), (Throws.ArgumentException));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(DebitProjectionCases))]
+        public void Test_Expenses_ProjectedCashFlow_GetProjectedDebitAt(
+            DateTime startAt,
+            DateTime endAt,
+            DateTime projectedAt,
+            decimal expectedAmount)
+        {
+            var cashFlow = new ProjectedCashFlow(startAt, endAt);
+
+            Assert.That(cashFlow.GetProjectedDebitAt(projectedAt), Is.EqualTo(expectedAmount));
+        }
+    }
+}
---
+++ Financier.Web/GraphQL/ProjectedCashFlows/ProjectedCashFlowQuery.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphQL;
+using GraphQL.Types;
+
+using Financier.Common.Expenses;
+
+namespace Financier.Web.GraphQL.CashFlows
+{
+    public class ProjectedCashFlowQuery : ObjectGraphType
+    {
+        public static class Keys
+        {
+            public static string Year = "year";
+            public static string Month = "month";
+
+            public static string FromYear = "fromYear";
+            public static string FromMonth = "fromMonth";
+            public static string ToYear = "toYear";
+            public static string ToMonth = "toMonth";
+
+            // public static string ToProjectYear = "toProjectedYear";
+            // public static string ToProjectMonth = "toProjectedMonth";
+        }
+
+        public ProjectedCashFlowQuery()
+        {
+            Field<ListGraphType<CashFlowType>>(
+                "getMonthlyCashFlows",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = Keys.FromYear
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = Keys.FromMonth
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = Keys.ToYear
+                    },
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = Keys.ToMonth
+                    }
+                ),
+                resolve: context =>
+                {
+                    var fromYear = context.GetArgument<int>(Keys.FromYear);
+                    var fromMonth = context.GetArgument<int>(Keys.FromMonth);
+                    var toYear = context.GetArgument<int>(Keys.ToYear);
+                    var toMonth = context.GetArgument<int>(Keys.ToMonth);
+
+                    var fromDate = new DateTime(fromYear, fromMonth, 1);
+                    var toDate = new DateTime(toYear, toMonth, 1);
+
+                    // Should this be converted to an array?
+                    return GetMonthlyAnalysis(fromDate, toDate).ToList();
+                }
+            );
+
+            Field<CashFlowType>(
+                "getYearlyCashFlow",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IntGraphType>>
+                    {
+                        Name = Keys.Year
+                    }
+                ),
+                resolve: context =>
+                {
+                    var year = context.GetArgument<int>(Keys.Year);
+
+                    return new YearlyCashFlow(year);
+                }
+            );
+        }
+
+        private IEnumerable<DurationCashFlow> GetMonthlyAnalysis(int year)
+        {
+            for (var month = 1; month <= 12; month += 1)
+            {
+                yield return new MonthlyCashFlow(year, month);
+            }
+        }
+
+        private IEnumerable<DurationCashFlow> GetMonthlyAnalysis(DateTime fromDate, DateTime toDate)
+        {
+            var date = new DateTime(fromDate.Year, fromDate.Month, 1);
+            var endDate = new DateTime(toDate.Year, toDate.Month, 1);
+
+            for (; date <= endDate; date = date.AddMonths(1))
+            {
+                System.Console.WriteLine($"{date.Year}.{date.Month}");
+                yield return new MonthlyCashFlow(date.Year, date.Month);
+            }
+        }
+    }
+}
