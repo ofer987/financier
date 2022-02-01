0f66aa9d353b3db7894ec2fa162a753fb45b8733
Dan Jakob Ofer <dan@ofer.to>
2022-02-01 05:56:28 -0500

    WIP

--- Financier.Web/Data/IdentityDataContext.cs
+++ Financier.Web/Data/IdentityDataContext.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
-namespace Financier.Web.Areas.Identity.Data;
+namespace Financier.Web.Data;
 
 public class IdentityDataContext : IdentityDbContext<IdentityUser>
 {
@@ -17,5 +18,21 @@ public class IdentityDataContext : IdentityDbContext<IdentityUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=financier.users;Username=ofer987");
+    }
+}
+
+public class IdentityContextFactory : IDesignTimeDbContextFactory<IdentityDataContext>
+{
+    public IdentityDataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<IdentityDataContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Database=financier.users;Username=ofer987");
+
+        return new IdentityDataContext(optionsBuilder.Options);
     }
 }
--- Financier.Web/Startup.cs
+++ Financier.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,7 @@ using Financier.Web.GraphQL.CashFlows;
 using Financier.Web.GraphQL.ItemQueries;
 using Financier.Web.GraphQL.Items;
 using Financier.Web.GraphQL.Activities;
+using Financier.Web.Data;
 
 namespace Financier.Web
 {
@@ -55,6 +57,9 @@ namespace Financier.Web
             services.AddSingleton<ItemQuerySchema>();
             services.AddSingleton<FixedRateMortgageSchema>();
             services.AddSingleton<OneHomeSchema>();
+            services.AddSingleton<IdentityDataContext>();
+            services.AddAuthenticationCore();
+            services.AddAuthorizationCore();
 
             // Add GraphQL
             services
@@ -94,6 +99,11 @@ namespace Financier.Web
                         .Build()
                 );
             });
+
+            services.AddLogging();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IdentityDataContext>();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -115,7 +125,6 @@ namespace Financier.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-
             app.UseGraphQLPlayground();
 
             app.UseRouting();
@@ -130,57 +139,15 @@ namespace Financier.Web
                 routes.MapGraphQL<OneHomeSchema>("/graphql/one-home");
             });
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // app.MapRazorPages();
+
             // Please note that some of these routes are deprecated
             // TODO: Remove the deprecated routes
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                    name: "Home",
-                    template: string.Empty,
-                    defaults: new { controller = "Home", action = "Index" }
-                );
-
-                routes.MapAreaRoute(
-                    name: "Expenses/MyHome#GetMonthlyPayments",
-                    areaName: "Expenses",
-                    template: "expenses/my-home/get-monthly-payments",
-                    defaults: new { controller = "MyHome", action = "GetMonthlyPayments" }
-                );
-
-                routes.MapRoute(
-                    name: "Items#Chart",
-                    template: "Items/chart",
-                    defaults: new { controller = "Charts", action = "Chart" }
-                );
-
-                routes.MapRoute(
-                    name: "CashFlows#GetYear",
-                    template: "CashFlows/year/{year}",
-                    defaults: new { controller = "CashFlows", action = "GetYear" }
-                );
-
-                routes.MapRoute(
-                    name: "CashFlows#GetMonth",
-                    template: "CashFlows/year/{year}/month/{month}",
-                    defaults: new { controller = "CashFlows", action = "GetMonth" }
-                );
-
-                routes.MapRoute(
-                    name: "AllItems#GetMonth",
-                    template: "AllItems/Get/year/{year}/month/{month}",
-                    defaults: new { controller = "AllItems", action = "GetMonth" }
-                );
-
-                routes.MapRoute(
-                    name: "AllItems#GetYear",
-                    template: "AllItems/Get/year/{year}",
-                    defaults: new { controller = "AllItems", action = "GetYear" }
-                );
-
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-
                 routes.MapRouteAnalyzer("/routes");
             });
         }
