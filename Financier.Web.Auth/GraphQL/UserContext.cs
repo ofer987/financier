1b296a0e5f3b13d819c63177d1bfc0a291094722
Dan Jakob Ofer <dan@ofer.to>
2022-08-06 17:07:55 -0400

    chore(Financier.Web.Auth): delete

--- Financier.Web.Auth/GraphQL/UserContext.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.Principal;
-
-namespace Financier.Web.Auth.GraphQL;
-
-public class UserContext : Dictionary<string, object>
-{
-    public IPrincipal User { get; init; } = new NullPrincipal();
-}
